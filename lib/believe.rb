841c9d76441e01e3d834574773bf3dae14a5d023
Chaitanya <cayadav2009@gmail.com>
2017-05-30 18:56:36 +0530

    Add code to Gem

--- lib/believe.rb
+++ lib/believe.rb
@@ -1,5 +1,12 @@
-require "believe/version"
+require 'believe/version'
+require 'engtagger'
+require 'dogeify'
 
+# This is amazing gem's beautiful class
 module Believe
   # Your code goes here...
+  def self.process(str)
+    dogeify = Dogeify.new
+    dogeify.process(str)
+  end
 end
--- spec/believe_spec.rb
+++ spec/believe_spec.rb
@@ -5,7 +5,22 @@ describe Believe do
     expect(Believe::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#process' do
+    let(:input) { 'My grandson gave me a sweater for Christmas' }
+    let(:output) { Believe.process(input) }
+
+    it 'converts to downcase' do
+      expect(output.downcase).to eq(output)
+    end
+
+    it 'combines nouns with doge adjective' do
+      expect(output).to match /so grandson./i
+      expect(output).to match /such sweater./i
+      expect(output).to match /very christmas./i
+    end
+
+    it 'always appends "wow."' do
+      expect(output).to end_with 'wow.'
+    end
   end
 end
